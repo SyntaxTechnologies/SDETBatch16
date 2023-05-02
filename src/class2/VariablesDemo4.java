7a264ced82537b87e47d8d1b8054b1dab421ada2
Aslan <128255111+AslanAslan01@users.noreply.github.com>
2023-05-02 14:27:47 -0400

    Delete src directory

--- src/class2/VariablesDemo4.java
+++
@@ -1,9 +0,0 @@
-package class2;
-
-public class VariablesDemo4 {
-    public static void main(String[] args) {
-
-      //  System.out.println(number);
-        int number=10;
-    }
-}
--- src/class2/VariablesDemo5.java
+++
@@ -1,8 +0,0 @@
-package class2;
-
-public class VariablesDemo5 {
-    public static void main(String[] args) {
-        int number;
-       // System.out.println(number);
-    }
-}
