ba009830042d04f6ac3de1f771e25433f8288852
Asghar <instructor@syntaxtechs.com>
2023-06-04 13:49:20 -0400

    class 28 Excel File reading

---
+++ src/main/java/class28/E7Task.java
@@ -0,0 +1,12 @@
+package class28;
+
+import java.util.Scanner;
+
+public class E7Task {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        scanner.nextInt();
+
+        // 1:50
+    }
+}
