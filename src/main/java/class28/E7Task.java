7109121d8ccfb367e0341ff86e79dc5db5d6ab38
Asghar <instructor@syntaxtechs.com>
2023-06-04 14:28:33 -0400

    class 28 functions

--- src/main/java/class28/E7Task.java
+++ src/main/java/class28/E7Task.java
@@ -1,12 +1,20 @@
 package class28;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class E7Task {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        scanner.nextInt();
 
-        // 1:50
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Please Enter an int");
+            scanner.nextInt();
+        } catch (InputMismatchException ime) {
+            System.out.println("Enter proper type of data");
+        }
     }
+
+
 }
