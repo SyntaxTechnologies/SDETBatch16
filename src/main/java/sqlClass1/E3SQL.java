44bb5ab64f9a54de4ae3d614bbf285acb090de0e
Asghar <instructor@syntaxtechs.com>
2023-07-25 21:51:56 -0400

    SQL Class

--- src/main/java/sqlClass1/E3SQL.java
+++ src/main/java/sqlClass1/E3SQL.java
@@ -3,16 +3,22 @@ package sqlClass1;
 import utils.ConfigReader;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class E2SQL {
+public class E3SQL {
     public static void main(String[] args) {
 
-        // break till 9:06
+
         String dbUrl = ConfigReader.getProperty("dbUrl");
         String dbUserName = ConfigReader.getProperty("dbUserName");
         String dbPassword = ConfigReader.getProperty("dbPassword");
+        List<Map<String,String>> empMapList= new ArrayList<>();
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+            connection = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
             Statement statement = connection.createStatement();
 
             String query = "select * from employee";
@@ -22,24 +28,22 @@ public class E2SQL {
             ResultSetMetaData metaData = resultSet.getMetaData();
             // gives us the no of columns in the resultSet
             int columnCount = metaData.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(metaData.getColumnLabel(i) + " ");
-            }
-            System.out.println();
-            // while loop goes throug the rows
+
             while (resultSet.next()){
-                // it goes through the columns
+                Map<String,String> rowMap=new HashMap<>();
                 for (int i = 1; i <=columnCount ; i++) {
-                    System.out.print(resultSet.getString(i)+" ");
+                    String key=metaData.getColumnLabel(i);
+                    String value=resultSet.getString(i);
+                    rowMap.put(key,value);
                 }
-                System.out.println();
+
+                empMapList.add(rowMap);
             }
-
-
 
         } catch (SQLException e) {
-            e.printStackTrace();
+          e.printStackTrace();
         }
 
+        System.out.println(empMapList);
     }
 }
